5c4433283adcf9e53f3036b8418219feff4a38de
parkksss <yesleee9211@gmail.com>
2022-03-08 22:06:55 +0900

    Jinja2 템플릿 언어 이용해서 서버사이드 렌더링 시도

---
+++ detail.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
+import requests
+
+app = Flask(__name__)
+
+client = MongoClient('mongodb://test:sparta@cluster0-shard-00-00.ite7q.mongodb.net:27017,cluster0-shard-00-01.ite7q.mongodb.net:27017,cluster0-shard-00-02.ite7q.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-1371nk-shard-0&authSource=admin&retryWrites=true&w=majority')
+db = client.dbsparta
+
+
+@app.route('/')
+def main():
+    # DB에서 저장된 단어 찾아서 HTML에 나타내기
+    return render_template("index.html")
+
+
+#detailpg.html로 연결하면서 mnt_no 데이터를 전송
+@app.route('/detail', methods=['GET'])
+def detail():
+    # index = 5 # 테스트
+    index = request.args.get('index')
+    museum = db.muse_info.find_one({'index': index})
+    return render_template('detail.html', museum = museum, index= index) #index 없애도 ...되려나..?
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
