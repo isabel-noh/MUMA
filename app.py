f40788ba60c725a8d89bc2eaa9c62306b8523712
seokjun <unbeatablesj@naver.com>
2022-03-08 21:16:05 +0900

    FEAT: 테스트 페이지 구동가능

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template, request, jsonify
+
+import config
+
 app = Flask(__name__)
 import certifi
 from pymongo import MongoClient
 
 import hashlib
 
-client = MongoClient('mongodb+srv://id:password@cluster0.0vjef.mongodb.net/Cluster0?retryWrites=true&w=majority',tlsCAFile=certifi.where())
+client = MongoClient(config.mongoDB)
 db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-    #   필터 기능을 3중으로...하는 중입니다.
+@app.route('/join')  # html join으로 통일 변경
+def join():
+    return render_template('join.html')
+
+@app.route('/login')  # html join으로 통일 변경
+def login():
+    return render_template('login.html')
+
+    #   필터 기능
 @app.route("/muse_select", methods=["GET"])
 def mnt_select():
     doc = []  # 검색을 마친 자료가 들어갈 배열입니다.
@@ -23,14 +34,43 @@ def mnt_select():
     museums = list(db.muse_info.find({}, {'_id': False}))  # 박물관의 전체 목록을 museums 변수로 받아옵니다.
     if builder_receive == '국공립':
         for museum in museums:
-            if museum["type"] == '국립' or museum["type"] =='공립':
+            if museum["type"] == '국립' or museum["type"] == '공립':
+                if type_receive == '박물관전체':
+                    if area_receive == '지역전체':
+                        doc.append(museum)
+                    elif area_receive in museum['addr']:
+                        doc.append(museum)
+                elif type_receive in museum['name']:
+                    if area_receive == '지역전체':
+                        doc.append(museum)
+                    elif area_receive in museum['addr']:
+                        doc.append(museum)
     elif builder_receive == '타입전체':
         for museum in museums:
-            doc.append(museum)
+            if type_receive == '박물관전체':
+                if area_receive == '지역전체':
+                    doc.append(museum)
+                elif area_receive in museum['addr']:
+                    doc.append(museum)
+            elif type_receive in museum['name']:
+                if area_receive == '지역전체':
+                    doc.append(museum)
+                elif area_receive in museum['addr']:
+                    doc.append(museum)
+
     else:
         for museum in museums:
             if builder_receive in museum["type"]:
-                doc.append(museum)
+                if type_receive == '박물관전체':  # str.contains('박물관')
+                    if area_receive == '지역전체':
+                        doc.append(museum)
+                    elif area_receive in museum['addr']:
+                        doc.append(museum)
+                elif type_receive in museum['name']:
+                    if area_receive == '지역전체':
+                        doc.append(museum)
+                    elif area_receive in museum['addr']:
+                        doc.append(museum)
 
     return jsonify({'search_list': doc, 'msg': '검색완료!'})
 
@@ -38,22 +78,18 @@ def mnt_select():
 @app.route('/detail_pg', methods=["GET"])
 def detail_pg():
     index = request.args.get('index')
-    return render_template('detailpg.html', index = index)
+    return render_template('detailpg.html', index=index)
 
 @app.route("/muse_info", methods=["GET"])
 def muse_get():
-    all_muse = list(db.muse_info.find({},{'_id':False}))
+    all_muse = list(db.muse_info.find({}, {'_id': False}))
     return jsonify({'mnt': all_muse})
 
 @app.route("/muse_show", methods=["GET"])
 def show_muse():
     num_receive = request.args.get('num_give')
-    muse_data = db.muse_info.find_one({'index':int(num_receive)},{'_id':False})
+    muse_data = db.muse_info.find_one({'index': int(num_receive)}, {'_id': False})
     return jsonify({'muse_data': muse_data})
-
-@app.route('/join') # html join으로 통일 변경
-def join():
-    return render_template('join.html')
 
 @app.route('/api/join', methods=['POST'])
 def api_join():
@@ -91,4 +127,4 @@ def api_checkNick():
         return jsonify({'result': 'failure', 'msg': '사용중인 닉네임입니다.'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
