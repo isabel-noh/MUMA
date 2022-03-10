caa9df777696a25989673753f2bafc5d73d30a41
seokjun <unbeatablesj@naver.com>
2022-03-10 11:10:06 +0900

    FEAT: 메인페이지 더보기 기능 추가

--- app.py
+++ app.py
@@ -169,9 +169,10 @@ def muse_search():
 # detailpg.html로 연결하면서 해당박물관 데이터를 전송
 @app.route('/detail/mm_no/<index>')
 def detail(index):
+    mm_no = index
     index = int(index)
     museum = db.muse_info.find_one({'index': index})
-
+    img = db.mimgs.find_one({'mm_no': mm_no})
     # 지도 주소 위도, 경도로 변환 : x, y 값
     headers = {
         "X-NCP-APIGW-API-KEY-ID": "afrl6tl1y2",
@@ -184,12 +185,73 @@ def detail(index):
         if len(response["addresses"]) > 0:
             x = float(response["addresses"][0]["x"])
             y = float(response["addresses"][0]["y"])
-            print(museum["addr"], x, y)
         else:
             print("좌표를 찾지 못했습니다")
-            # print(title, "좌표를 찾지 못했습니다")
 
-    return render_template('detail.html', museum = museum, addr_x = x, addr_y = y)
+    return render_template('detail.html', museum = museum, img=img, addr_x = x, addr_y = y)
+
+@app.route('/saveimg', methods=['POST'])
+def saveimg():
+    mmnum_receive = request.form["mmnum_give"]
+    file = request.files["file_give"]
+
+    extension = file.filename.split('.')[-1]
+
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+    filename = f'file-{mytime}'
+    save_to = f'static/{filename}.{extension}'
+    file.save(save_to)
+
+    doc = {
+        'mm_no': mmnum_receive,
+        'file': f'{filename}.{extension}'
+    }
+
+    db.mimgs.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
+
+
+@app.route('/posting', methods=['POST'])
+def posting():
+    mmnum_receive = request.form["mmnum_give"]
+    name_receive = request.form["name_give"]
+    score_receive = request.form["score_give"]
+    comment_receive = request.form["comment_give"]
+    date_receive = request.form["date_give"]
+    doc = {
+        "m_num": mmnum_receive,
+        "username": name_receive,
+        "score": score_receive,
+        "comment": comment_receive,
+        "date": date_receive
+    }
+    db.posts.insert_one(doc)
+    return jsonify({"result": "success", 'msg': '포스팅 성공'})
+
+@app.route("/get_posts", methods=['GET'])
+def get_posts():
+    m_num = request.args.get('num_give')
+    posts = list(db.posts.find({'m_num': m_num}).sort("date", -1))
+    for post in posts:
+        post["_id"] = str(post["_id"])
+    return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts": posts})
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    # 삭제하기
+    m_num = request.args.get('num_give')
+    post_id_receive = request.form["post_id_give"]
+    posts = list(db.posts.find({'m_num': m_num}).sort("date", -1))
+    for post in posts:
+        if str(post["_id"]) == post_id_receive:
+            db.posts.delete_one({"_id":post["_id"]})
+    return jsonify({'result': 'success', 'msg': 'deleted'})
+
+# @app.route('/delete_post', methods=['DELETE'])
+# def delete_post():
 
 @app.route("/muse_info", methods=["GET"])
 def muse_get():
@@ -266,7 +328,6 @@ def api_login():
                 'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 4)  # 로그인 4시간 유지
             }
             acc_token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-            print(acc_token)
             return jsonify({'result': 'success', 'acc_token': acc_token})
 
     # 찾지 못하면
